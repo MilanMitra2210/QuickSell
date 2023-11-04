84808c7a54db8ebb7479ad8087543b6b81439c52
Milan Mitra <milanmitranda2020@gmail.com>
2023-11-04 21:57:45 +0530

    Initial Commit

---
+++ frontend-assignment/src/App.js
@@ -0,0 +1,11 @@
+import TopFold from './components/TopBar';
+
+function App() {
+  return (
+    <>
+      <TopFold/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend-assignment/src/components/TopBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopFold = () => {
+  return (
+    <div>
+      hello wirld
+    </div>
+  )
+}
+
+export default TopFold
